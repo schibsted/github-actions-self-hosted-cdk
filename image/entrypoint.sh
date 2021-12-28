49caf0eff31222fa55a76593fa2d721f15ade67c
Henric Englund <hnrcnglnd@gmail.com>
2021-12-28 10:04:18 +0100

    wip

--- image/entrypoint.sh
+++ image/entrypoint.sh
@@ -8,14 +8,16 @@ if [[ -z "${GITHUB_TOKEN}" || -z "${RUNNER_CONTEXT}" ]]; then
   exit 1
 else
   AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
-  USERNAME=$(cut -d/ -f4 <<< ${RUNNER_CONTEXT})
-  REPOSITORY=$(cut -d/ -f5 <<< ${RUNNER_CONTEXT})
+  #USERNAME=$(cut -d/ -f4 <<< ${RUNNER_CONTEXT})
+  #USERNAME=${RUNNER_CONTEXT}
+  USERNAME=spt-mediaplatform-labs
+  #REPOSITORY=$(cut -d/ -f5 <<< ${RUNNER_CONTEXT})
 
-  if [[ -z "${REPOSITORY}" ]]; then 
-    TOKEN_REGISTRATION_URL="https://api.github.com/orgs/${USERNAME}/actions/runners/registration-token"
-  else
-    TOKEN_REGISTRATION_URL="https://api.github.com/repos/${USERNAME}/${REPOSITORY}/actions/runners/registration-token"
-  fi
+#  if [[ -z "${REPOSITORY}" ]]; then 
+    TOKEN_REGISTRATION_URL="https://github.schibsted.io/api/v3/orgs/${USERNAME}/actions/runners/registration-token"
+#  else
+#    TOKEN_REGISTRATION_URL="https://github.schibsted.io/api/v3/repos/${USERNAME}/${REPOSITORY}/actions/runners/registration-token"
+#  fi
     
   RUNNER_TOKEN="$(curl -XPOST -fsSL \
     -H "Accept: application/vnd.github.v3+json" \
@@ -24,5 +26,6 @@ else
     | jq -r '.token')"
 fi
 
-./config.sh --url "${GITHUB_ACTIONS_RUNNER_CONTEXT}" --token "${RUNNER_TOKEN}" --name "${RUNNER_NAME}" --work "${RUNNER_WORKDIR}"
+echo "Configuring..."
+./config.sh --url "https://github.schibsted.io/spt-mediaplatform-labs" --token "${RUNNER_TOKEN}" --name "${RUNNER_NAME}" --work "${RUNNER_WORKDIR}"
 ./run.sh
