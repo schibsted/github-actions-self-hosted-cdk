5afc15205e07017f2b81722d579ee2b292e27c5c
Henric Englund <hnrcnglnd@gmail.com>
2021-12-30 23:10:29 +0100

    Make it possible to have repo based contexts

--- image/entrypoint.sh
+++ image/entrypoint.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 
-RUNNER_NAME=${RUNNER_NAME:-default}
 RUNNER_WORKDIR=${RUNNER_WORKDIR:-_work}
 
 if [[ -z "${GITHUB_TOKEN}" || -z "${RUNNER_CONTEXT}" ]]; then
-  echo 'One of the mandatory parameters is missing. Quit!'
+  echo 'One of the mandatory parameters is missing. Quitting...'
   exit 1
 else
   AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
-  #USERNAME=$(cut -d/ -f4 <<< ${RUNNER_CONTEXT})
-  #USERNAME=${RUNNER_CONTEXT}
-  USERNAME=spt-mediaplatform-labs
-  #REPOSITORY=$(cut -d/ -f5 <<< ${RUNNER_CONTEXT})
+  ORG=$(cut -d/ -f1 <<< ${RUNNER_CONTEXT})
+  REPO=$(cut -d/ -f2 <<< ${RUNNER_CONTEXT})
 
-#  if [[ -z "${REPOSITORY}" ]]; then 
-    TOKEN_REGISTRATION_URL="https://github.schibsted.io/api/v3/orgs/${USERNAME}/actions/runners/registration-token"
-#  else
-#    TOKEN_REGISTRATION_URL="https://github.schibsted.io/api/v3/repos/${USERNAME}/${REPOSITORY}/actions/runners/registration-token"
-#  fi
+  if [[ -z "${REPOSITORY}" ]]; then
+    TOKEN_REGISTRATION_URL="https://github.schibsted.io/api/v3/orgs/${ORG}/actions/runners/registration-token"
+  else
+    TOKEN_REGISTRATION_URL="https://github.schibsted.io/api/v3/repos/${ORG}/${REPO}/actions/runners/registration-token"
+  fi
     
   RUNNER_TOKEN="$(curl -XPOST -fsSL \
     -H "Accept: application/vnd.github.v3+json" \
@@ -27,5 +24,5 @@ else
 fi
 
 echo "Configuring..."
-./config.sh --url "https://github.schibsted.io/spt-mediaplatform-labs" --token "${RUNNER_TOKEN}" --work "${RUNNER_WORKDIR}" --ephemeral
+./config.sh --url "https://github.schibsted.io/${RUNNER_CONTEXT}" --token "${RUNNER_TOKEN}" --work "${RUNNER_WORKDIR}" --ephemeral
 ./run.sh
