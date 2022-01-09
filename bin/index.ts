c42e0b5ca32a4211e41470e5d92084bdb8aafca1
Henric Englund <hnrcnglnd@gmail.com>
2022-01-09 21:52:36 +0100

    lib => src

--- bin/index.ts
+++ bin/index.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import { App } from 'aws-cdk-lib';
-import { GithubActionsRunnerStack } from '../lib';
+import { GithubActionsRunnerStack } from '../src';
 
 const app = new App();
 new GithubActionsRunnerStack(app, 'GithubActionsRunnerStack', {
